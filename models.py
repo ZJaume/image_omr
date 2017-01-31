6f8365e92a4f79f7fceb52684a670835c4f34f5a
ZJaume <retrasado.man@gmail.com>
2017-01-31 19:17:56 +0100

    Changed model to functional

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.layers import Input, Dense, Activation
 from keras.layers import Reshape, Lambda
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers.recurrent import GRU
 from keras.layers.wrappers import Bidirectional
 from keras.optimizers import SGD
@@ -19,12 +19,12 @@ def ctc_lambda_func(args):
 #
 # Create RNN with convolutional filters and CTC logloss function
 #
-def create_rnn(input_shape):
+def create_rnn(input_shape, lb_max_length, nb_classes):
     if K.image_dim_ordering() =='th':
         img_w = input_shape[1]
         img_h = input_shape[2]
     else:
-        img_w = input_shape[2]
+        img_w = input_shape[0]
         img_h = input_shape[1]
 
     # Network parameters
@@ -36,29 +36,43 @@ def create_rnn(input_shape):
     output_size = 28
     act = 'relu'
 
-    model = Sequential()
-    model.add(Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
-                activation=act, init='he_normal', name='conv1', input_shape=input_shape))
-    model.add(MaxPooling2D(pool_size=(pool_size, pool_size), name='max1'))
-    model.add(Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
-                activation=act, init='he_normal', name='conv2'))
-    model.add(MaxPooling2D(pool_size=(pool_size, pool_size), name='max2'))
+    input_data = Input(name='the_input', shape=input_shape, dtype='float32')
+    inner = Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
+                          activation=act, init='he_normal', name='conv1')(input_data)
+    inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max1')(inner)
+    inner = Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
+                          activation=act, init='he_normal', name='conv2')(inner)
+    inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max2')(inner)
 
     conv_to_rnn_dims = (img_w // (pool_size ** 2), (img_h // (pool_size ** 2)) * conv_num_filters)
-    model.add(Reshape(target_shape=conv_to_rnn_dims, name='reshape'))
-    model.add(Dense(time_dense_size, activation=act, name='dense1'))
+    inner = Reshape(target_shape=conv_to_rnn_dims, name='reshape')(inner)
 
-    model.add(Bidirectional(GRU(rnn_size, return_sequences=True, init='he_normal', name='gru1')))
-    model.add(Bidirectional(GRU(rnn_size, return_sequences=True, init='he_normal', name='gru2')))
+    # cuts down input size going into RNN:
+    inner = Dense(time_dense_size, activation=act, name='dense1')(inner)
 
-    model.add(Dense(output_size, init='he_normal',name='dense2'))
-    model.add(Activation('softmax', name='softmax'))
+    # Two layers of bidirecitonal GRUs
+    # GRU seems to work as well, if not better than LSTM:
+    inner = Bidirectional(GRU(rnn_size, return_sequences=True, init='he_normal', name='gru1'))(inner)
+    inner = Bidirectional(GRU(rnn_size, return_sequences=True, init='he_normal', name='gru2'))(inner)
 
-    model.add(Lambda(ctc_lambda_func, output_shape=(1,), name='ctc'))
+    # transforms RNN output to character activations:
+    inner = Dense(nb_classes, init='he_normal',
+                  name='dense2')(inner)
+    y_pred = Activation('softmax', name='softmax')(inner)
+    Model(input=[input_data], output=y_pred).summary()
+
+    labels = Input(name='the_labels', shape=[lb_max_length], dtype='float32')
+    input_length = Input(name='input_length', shape=[1], dtype='int64')
+    label_length = Input(name='label_length', shape=[1], dtype='int64')
+    # Keras doesn't currently support loss funcs with extra parameters
+    # so CTC loss is implemented in a lambda layer
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
+
+    # clipnorm seems to speeds up convergence
     sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
-    model.compile(loss='ctc', optimizer=sgdi, metricss=['accuracy'])
 
-    model.summary()
+    model = Model(input=[input_data, labels, input_length, label_length], output=[loss_out])
+    model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
+
     return model
 
-model = create_rnn((1,457,136))
