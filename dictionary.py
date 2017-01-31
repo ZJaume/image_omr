59cd5a4909cccfeb4282131eeb8775abfc0f5e6b
ZJaume <retrasado.man@gmail.com>
2017-01-31 18:06:37 +0100

    Dictionary fills the labels

--- dictionary.py
+++ dictionary.py
@@ -9,29 +9,36 @@ dict_path = "./data/lilypond/dictionary.json"
 #
 def create_dictionary():
     categories = []
+    max_length = 0
     print("Creating dictionary...")
 
     for path in paths:
         f = open(path + "labels.txt")
         for line in f:
-            for token in line.split():
+            for (i,token) in enumerate(line.split()):
                 if token not in categories:
                     categories.append(token)
+                if i > max_length:
+                    max_length = i
 
     words = dict()
     for i,cat in enumerate(categories):
         words[cat] = i
 
-    print("Dictionary has " + str(len(words)) + " words")
+    dict_length = len(words)
+    print("Dictionary has " + str(dict_length) + " words")
+    max_length += 1
+    print("Maximum length is " + str(max_length))
     with open(dict_path,'w') as fp:
         json.dump(words, fp, sort_keys=True, indent=4)
 
     print("---> Succesfully imported to JSON format")
+    return max_length, dict_length
 
 #
 # Translate the labels to a coded format using the dictionary
 #
-def codify():
+def codify(max_length, dict_length):
     words = None
     print("Codifying labels...")
     with open(dict_path,'r') as fp:
@@ -41,8 +48,13 @@ def codify():
         f = open(path + "labels.txt")
         aux = ""
         for line in f:
-            for token in line.split():
-                aux = aux + str(words[token]) + " "
+            tokens = line.split()
+            for i in range(max_length):
+                if i < len(tokens):
+                    aux = aux + str(words[tokens[i]]) + ' '
+                else:
+                    aux = aux + str(dict_length) + ' '
+
             aux = aux + '\n'
 
         f = open(path + "labels_cod.txt", 'w')
@@ -50,5 +62,5 @@ def codify():
     print("---> Succesfully codified labels")
 
 
-create_dictionary()
-codify()
+max_length, dict_length = create_dictionary()
+codify(max_length, dict_length)
