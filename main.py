686a9d24a6fc2f0302c79bf5e4646a1cfef2e64c
ZJaume <retrasado.man@gmail.com>
2017-02-02 19:55:59 +0100

    Renamed cols and rows

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ pool_size = 2
 lb_max_length = 15
 
 # Size of the images
-img_rows, img_cols = 180, 80
+img_w, img_h= 180, 80
 
 #
 # Load data function, recieves downsample factor equal to pool size
@@ -30,7 +30,7 @@ def load_data(downsample_factor):
         path = './data/lilypond/{}/'.format(directory)
         labels = open(path + 'labels_cod.txt')
         for filename in glob.glob(path + '*.png'):
-            im=Image.open(filename).resize((img_rows,img_cols)).convert('L')
+            im=Image.open(filename).resize((img_w,img_h)).convert('L')
             im=ImageOps.invert(im)      # Meaning of grey level is 255 (black) and 0 (white)
             label = np.fromstring(labels.readline(), dtype=int, sep=' ')
             label_length.append(len(label))
@@ -39,15 +39,15 @@ def load_data(downsample_factor):
 
     n = len(image_list)     # Total examples
     if K.image_dim_ordering() == 'th':
-        input_shape = (1, img_rows, img_cols)
-        X = np.asarray(image_list).reshape(n, 1, img_rows, img_cols)
+        input_shape = (1, img_w, img_h)
+        X = np.asarray(image_list).reshape(n, 1, img_w, img_h)
     else:
-        input_shape = (img_rows, img_cols, 1)
-        X = np.asarray(image_list).reshape(n, img_rows, img_cols, 1)
+        input_shape = (img_w, img_h, 1)
+        X = np.asarray(image_list).reshape(n, img_w, img_h, 1)
 
     inputs = { 'the_input' : X,
                 'the_labels' : np.asarray(class_list),
-                'input_length' : np.full((n,), img_cols // downsample_factor ** 2 - 2),
+                'input_length' : np.full((n,), img_w // downsample_factor ** 2 - 2),
                 'label_length' : np.asarray(label_length),
             }
     outputs = {'ctc' : np.zeros((n,), dtype=int)}
@@ -58,6 +58,7 @@ X, Y, input_shape = load_data(pool_size)
 
 print(str(Y['ctc'].shape[0]) + " trainning examples")
 print(str(nb_epoch) + " epochs")
+print(str(X['the_labels'].shape))
 
 model = models.create_rnn(input_shape, lb_max_length, nb_classes)
 model.fit(X, Y['ctc'], batch_size=batch_size, nb_epoch=nb_epoch, validation_split=0.2)
