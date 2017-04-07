934f52c7867409bad17f1eb0c7c8e6be6a18289c
ZJaume <retrasado.man@gmail.com>
2017-04-07 10:46:12 +0200

    Added border to the stave images

--- semisynthetic_generator.py
+++ semisynthetic_generator.py
@@ -7,6 +7,7 @@ black = 0
 white = 255
 
 homus_size = 40
+border = 10
 
 #
 # Function that generates a music stave, lines-blanks ratio is 20% 80% by default
@@ -15,20 +16,22 @@ def gen_stave(width, heigth):
     blank_size = int(heigth*0.80)//4
     line_size = int(heigth*0.20)//5
 
-    im = Image.new('L',(width,heigth), color=white)
+    im = Image.new('L',(width,heigth + border*2), color=white)
     draw = ImageDraw.Draw(im)
-    x = 0
-    while x < heigth:
+    x = 0 + border
+    num_lines = 0
+    while x <= heigth + border*2 and num_lines < 5:
         draw.line([(0,x),(width,x)], fill=black, width=line_size)
         x += line_size + blank_size
-    im.show()
+        num_lines+=1
+    #im.show()
     return im
 
 #
 # Given a list of images of symbols, paste it on the stave
 #
 def put_symbols(stave, symbols, offset=0):
-    box = [0,int(homus_size*0.1)]
+    box = [0,int(homus_size*0.1) + border]
     for img in imgs:
         stave.paste(img, box=tuple(box), mask=ImageOps.invert(img))
         box[0] += homus_size
@@ -38,6 +41,6 @@ imgs = []
 for f in glob.glob("./data/A*.jpg"):
     imgs.append(Image.open(f))
 
-stave = gen_stave(homus_size*len(imgs),int(homus_size*1.2))
+stave = gen_stave(homus_size*len(imgs),int(homus_size*1.6))
 put_symbols(stave, imgs)
-stave.show()
+stave.show(command='eog')
