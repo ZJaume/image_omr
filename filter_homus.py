4969678a1ea096175c30bab28f752202f8522681
ZJaume <retrasado.man@gmail.com>
2017-06-07 17:28:14 +0200

    New utility to filter original homus, and the HOMUS converted to png

--- filter_homus.py
+++ filter_homus.py
@@ -24,6 +24,12 @@ def time_filter(paths):
     regex = r'([A-Za-z0-9 -_\/]+((2-2)|(3-8)|(9-8)|(12-8))-Time.*)'
     return [path for path in paths if not re.match(regex,elem)]
 
-img = cv2.imread(sys.argv[1])
-img = dilate(img, (3,3), 2)
+imgs = []
+for i in range(1,5):
+    imgs.extend(glob.glob('./data/HOMUS_4Fold/F{}/*'.format(i)))
 
+for img in imgs:
+    mat = cv2.imread(img,0)
+    name = re.sub(r'\.pbm','',re.sub(r'\./data/HOMUS_4Fold/','',img))
+    print(dest+name+'.png')
+    cv2.imwrite(dest + name + '.png',mat,[9])
