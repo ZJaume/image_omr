c941e80af643663c21e835831314d627de4db7ce
ZJaume <retrasado.man@gmail.com>
2017-06-12 13:45:33 +0200

    Reading filepaths and labels in numpy arrays for more flexible data loading

--- main.py
+++ main.py
@@ -38,10 +38,10 @@ def load_data(downsample_factor, paths, labels):
     label_length = []
     num_examples = batch_size*90
 
-    for filename in paths:
+    for filename, lb in zip(paths, labels):
         im=Image.open(filename).resize((img_w,img_h)).convert('L')
         im=ImageOps.invert(im)      # Meaning of grey level is 255 (black) and 0 (white)
-        label = np.fromstring(labels.readline(), dtype=int, sep=' ')
+        label = np.fromstring(lb, dtype=int, sep=' ')
         label_length.append(len(label))
         fill = np.full((lb_max_length - len(label),), -1, dtype=int)
         class_list.append(np.append(label,fill))
@@ -69,38 +69,42 @@ def load_data(downsample_factor, paths, labels):
     # label_length = label_length[num_examples:]
     # n = num_examples
 
-    # Divide in train and test data
-    randomize = np.arange(n)
-    np.random.shuffle(randomize)
-    X, class_list, label_length = X[randomize], class_list[randomize], label_length[randomize]
-    n_partition = int(n*0.9)    # 10% validation
-
-    inputs_train = { 'the_input' : X[:n_partition],
-                'the_labels' : class_list[:n_partition],
-                'input_length' : np.full((n_partition,), img_w // downsample_factor ** 2 - 2),
-                'label_length' : label_length[:n_partition],
+    inputs_train = { 'the_input' : X,
+                'the_labels' : class_list,
+                'input_length' : np.full((n,), img_w // downsample_factor ** 2 - 2),
+                'label_length' : label_length,
                 }
-    inputs_test = { 'the_input' : X[n_partition:],
-                'the_labels' : class_list[n_partition:],
-                'input_length' : np.full((n-n_partition,), img_w // downsample_factor ** 2 - 2),
-                'label_length' : label_length[n_partition:],
-                }
-    outputs_train = {'ctc' : np.zeros((n_partition,), dtype=int)}
-    outputs_test = {'ctc' : np.zeros((n-n_partition,), dtype=int)}
+    outputs_train = {'ctc' : np.zeros((n,), dtype=int)}
 
-    return inputs_train, inputs_test, outputs_train, outputs_test, input_shape
+    return inputs_train, outputs_train, input_shape
 
 def sort_paths(num_paths, prefix, suffix, offset=0):
     sorted_paths = []
-    for i in range(1+offset, num_paths+offset+1):
-        sorted_paths.append(prefix + str(i) + suffix)
+    for i in range(num_paths):
+        sorted_paths.append(prefix + str(i+1) + suffix)
     return sorted_paths
 
-num_paths = len(glob.glob(path + '*.png'))
-paths = sort_paths(num_paths, path, '.png')
-labels = open(path + 'labels_cod.txt')
+def shuffle(a, b):
+    randomize = np.arange(a.shape[0])
+    np.random.shuffle(randomize)
+    return a[randomize], b[randomize]
 
-X_train, X_test, Y_train, Y_test, input_shape = load_data(pool_size, paths, labels)
+fp = open(path + 'labels_cod.txt', 'r')
+labels = fp.read().split('\n')
+if labels[-1] == '':
+    labels.pop()
+fp.close()
+num_paths = len(glob.glob(path + '*.png'))
+paths = np.asarray(sort_paths(num_paths, path, '.png'))
+labels = np.asarray(labels)
+
+# Divide in train and test data
+n_partition = int(num_paths*0.9)    # 10% validation
+paths, labels = shuffle(paths, labels)
+print(paths, labels)
+
+X_train, Y_train, input_shape = load_data(pool_size, paths[n_partition:], labels[n_partition:])
+X_test, Y_test, input_shape = load_data(pool_size, paths[:n_partition], labels[:n_partition])
 
 print(str(len(X_train['the_input'])) + " trainning examples")
 print(str(len(X_test['the_input'])) + " test examples")
